a6506cb787441fbd32869614b112a5bde2ccf892
jangidRkt08 <itsmejangid.rk@gmail.com>
2025-02-02 15:54:04 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+import Navbar from "./components/Navbar/Navbar";
+import Hero from "./components/Hero/Hero";
+import "./App.css";
+import Brands from "./components/Brands/Brands";
+import Services from "./components/Services/Services";
+import Banner from "./components/Banner/Banner";
+import Banner2 from "./components/Banner/Banner2";
+import Testimonial from "./components/Testimonial/Testimonial";
+import Newsletter from "./components/Newsletter/Newsletter";
+import Footer from "./components/Footer/Footer";
+
+function App() {
+  return (
+    <main className="overflow-x-hidden">
+      <Navbar />
+      <Hero />
+      <Brands />
+      <Services />
+      <Banner />
+      <Banner2 />
+      <Testimonial />
+      <Newsletter />
+      <Footer />
+    </main>
+  );
+}
+
+export default App;
